2ac47cb6745cd40db0c5deae05f4b441a417b085
ObedQ <obedotu51@gmail.com>
2021-11-17 23:23:34 +0000

    update

--- App.jsx
+++ App.jsx
@@ -1,17 +1,53 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
+import AppLoading from "expo-app-loading";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Screens } from "./navigations/screens";
 import { Home } from "./screens/Home";
 import { Login } from "./screens/Login";
+import { ProductDetail } from "./screens/ProductDetail";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_600SemiBold,
+  Poppins_700Bold,
+  Poppins_800ExtraBold,
+} from "@expo-google-fonts/poppins";
 
 const Stack = createNativeStackNavigator();
 export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName={"Login"}>
-        <Stack.Screen name='Home' component={Home} />
-        <Stack.Screen name='Login' component={Login} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+  let [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_600SemiBold,
+    Poppins_700Bold,
+    Poppins_800ExtraBold,
+  });
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else
+    return (
+      <SafeAreaProvider>
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName={Screens.Login}>
+            <Stack.Screen
+              name={Screens.Home}
+              component={Home}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name={Screens.Login}
+              component={Login}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name={Screens.ProductDetail}
+              component={ProductDetail}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </SafeAreaProvider>
+    );
 }
